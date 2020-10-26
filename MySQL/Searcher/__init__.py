c7ef1408f0c146e1001ca0d10aa4c20ac59be9d4
Cikecike <35445497+Cikecike@users.noreply.github.com>
2020-10-26 13:57:59 +0800

    Flask

--- MySQL/Searcher/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*-coding:utf-8-*-
-"""
-@author_    :   HP
-@date       :   2020/10/21 19:29
-@email      :   tsz1216@sina.com
-@software   :   PyCharm
-"""
--- MySQL/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*-coding:utf-8-*-
-"""
-@author_=LegalHigh"
-@date: 2020/10/21 11:48
-@email:tsz1216@sina.com
-"""
